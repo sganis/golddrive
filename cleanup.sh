9e8b5551cf52406899c97590443aea7f4bb25017
San <sganis@gmail.com>
2018-09-01 23:35:42 +0300

    Added .vs as cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-find . -type d -name "__pycache__" -exec rm -r "{}" \; 2>/dev/null
-find . -type d -name "x64" -exec rm -r "{}" \; 2>/dev/null
+find . -type d -name "__pycache__" -exec rm -r "{}" \;
+find . -type d -name "x64" -exec rm -r "{}" \; 
+find . -type d -name ".vs" -exec rm -r "{}" \; 
 git clean -dfx
 
