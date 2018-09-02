0da9d03425e47a83a82262348272b8e57b426219
San <sganis@gmail.com>
2018-09-02 15:04:15 +0300

    Fixed ssh authentication

--- release/app/app.py
+++ release/app/app.py
@@ -2,47 +2,42 @@
 # -*- coding: utf-8 -*-
 
 from PyQt5.QtWidgets import (QWidget, QLabel, 
-    QComboBox, QApplication)
+    QComboBox, QApplication, QMainWindow)
 import sys
+from app_ui import Ui_MainWindow
 
-class Example(QWidget):
+class Window(Ui_MainWindow, QMainWindow):
     
     def __init__(self):
         super().__init__()
-        
-        self.initUI()
-        
-        
-    def initUI(self):      
-
-        self.lbl = QLabel("Ubuntu", self)
-
-        combo = QComboBox(self)
-        combo.addItem("Ubuntu")
-        combo.addItem("Mandriva")
-        combo.addItem("Fedora")
-        combo.addItem("Arch")
-        combo.addItem("Gentoo")
-
-        combo.move(50, 50)
-        self.lbl.move(50, 150)
-
-        combo.activated[str].connect(self.onActivated)        
-         
-        self.setGeometry(300, 300, 300, 200)
-        self.setWindowTitle('QComboBox')
+        self.setupUi(self)
         self.show()
         
-        
-    def onActivated(self, text):
-      
-        self.lbl.setText(text)
-        self.lbl.adjustSize()  
+    # def initUI(self):      
+
+    #     self.lbl = QLabel("Ubuntu", self)
+
+    #     combo = QComboBox(self)
+    #     combo.addItem("Ubuntu")
+    #     combo.addItem("Mandriva")
+    #     combo.addItem("Fedora")
+    #     combo.addItem("Arch")
+    #     combo.addItem("Gentoo")
+
+    #     combo.move(50, 50)
+    #     self.lbl.move(50, 150)
+
+    #     combo.activated[str].connect(self.onActivated)        
+         
+    #     self.setGeometry(300, 300, 300, 200)
+    #     self.setWindowTitle('QComboBox')
+    #     self.show()
+
         
 def run():
-    # input('hello world')
+    
     app = QApplication(sys.argv)
-    ex = Example()
+    ex = Window()
     sys.exit(app.exec_())
 
 
--- release/app/setupssh.py
+++ release/app/setupssh.py
@@ -5,13 +5,13 @@ import util
 
 DIR = os.path.dirname(os.path.realpath(__file__))
 
-def test(ssh, userhost):
+def test(ssh, userhost, port=22):
 
-	cmd = f'"{ssh}" -o StrictHostKeyChecking=no -o BatchMode=yes {userhost} echo ok 2>&1'
+	cmd = f'"{ssh}" -p {port} -o StrictHostKeyChecking=no -o BatchMode=yes {userhost} echo ok 2>&1'
 	ok, err, ret = util.run(cmd)
 	return ok == 'ok'
 
-def main(ssh, keygen, userhost):
+def main(ssh, keygen, userhost, port=22):
 
 	print(f'Checking ssh keys for {userhost}...')
 
@@ -26,7 +26,7 @@ def main(ssh, keygen, userhost):
 		os.system(cmd)
 	else:
 		print('Private key already exists.')
-		if test(ssh, userhost):
+		if test(ssh, userhost, port):
 			return
 
 	print(f'Publising public key...\n')
@@ -37,9 +37,10 @@ def main(ssh, keygen, userhost):
 		key = f.read().strip()
 
 	# Copy these keys to the target machines.
-	cmd = f'echo y | "{ssh}" -o StrictHostKeyChecking=no {userhost} "mkdir -p .ssh && echo \'{key}\' >> .ssh/authorized_keys && chmod 700 .ssh/authorized_keys"'
-	util.run(cmd)
-	if test(ssh, userhost):
+	cmd = f'echo y | "{ssh}" -p {port} -o StrictHostKeyChecking=no {userhost} "mkdir -p .ssh && echo \'{key}\' >> .ssh/authorized_keys && chmod 700 .ssh/authorized_keys"'
+	print(cmd)
+	os.system(cmd)
+	if test(ssh, userhost, port):
 		print("SSH setup successfull")
 	else:
 		print('SSH setup failed')
@@ -49,6 +50,9 @@ if __name__ == '__main__':
 	import sys
 	assert len(sys.argv) > 1 and '@' in sys.argv[1] # usage: prog user@host
 	userhost = sys.argv[1]
+	port=22
+	if ':' in userhost:
+		userhost, port = userhost.split(':')		
 	ssh = r'C:\Program Files\SSHFS-Win\bin\ssh.exe'
 	keygen = r'C:\Program Files\SSHFS-Win\bin\ssh-keygen.exe'
-	main(ssh, keygen, userhost)
+	main(ssh, keygen, userhost, port)
