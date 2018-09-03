e8073c3cbf2213768609c0cc2dc419987d3a603e
San <sganis@gmail.com>
2018-09-03 13:39:37 +0300

    Improved ssh key setup with paramiko

--- release/app/setupssh.py
+++ release/app/setupssh.py
@@ -6,16 +6,15 @@ import paramiko
 
 DIR = os.path.dirname(os.path.realpath(__file__))
 
-def test(ssh, userhost, port=22):
+def checkssh(ssh, userhost, port=22):
 
 	cmd = f'"{ssh}" -p {port} -o StrictHostKeyChecking=no -o BatchMode=yes {userhost} echo ok 2>&1'
 	r = subprocess.run(cmd, capture_output=True, shell=True, text=True)
 	return r.stdout.strip() == 'ok'
 
-def main(ssh, keygen, userhost, password, port=22):
+def main(ssh, userhost, password, port=22):
 
 	print(f'Checking ssh keys for {userhost}...')
-
 	sshdir = os.path.expandvars("%USERPROFILE%\\.ssh")
 	seckey = os.path.join(sshdir, 'id_rsa')
 	pubkey = os.path.join(sshdir, 'id_rsa.pub')
@@ -27,43 +26,34 @@ def main(ssh, keygen, userhost, password, port=22):
 		print('Generating new ssh keys...')
 		sk = paramiko.RSAKey.generate(2048)
 		sk.write_private_key_file(seckey)	
-		# cmd = f'mkdir {sshdir} 2>nul && echo y | "{keygen}" -f {seckey} -q -N ""'
-		# os.system(cmd)
 	else:
 		print('Private key already exists.')
-		if test(ssh, userhost, port):
-			return
+		if checkssh(ssh, userhost, port):
+			print('SSH authentication is ok.')
+			return 0
 
-	print(f'Publising public key...\n')
-
-	# Generate and read the new keys into a variable
+	# Generate public key
+	print(f'Publising public key...')
 	if not sk:
 		sk = paramiko.RSAKey.from_private_key_file(seckey)
 	key = f'ssh-rsa {sk.get_base64()} {userhost}'
-	# todo: compare 	key with the existing 
-	# with open(pubkey, 'w') as w:
-	# 	w.write(key)
-
-	# cmd = f'"{keygen}" -y -f {seckey} > {pubkey}'
 	# with open(f'{pubkey}', 'r') as f:
 	# 	key = f.read().strip()
 
-	# Copy these keys to the target machines.
+	# Copy to the target machines.
 	cmd = f"mkdir -p .ssh && echo '{key}' >> .ssh/authorized_keys && chmod 700 .ssh/authorized_keys"
-	# cmd = f'"{ssh}" -p {port} -o StrictHostKeyChecking=no {userhost} "mkdir -p .ssh && echo \'{key}\' >> .ssh/authorized_keys && chmod 700 .ssh/authorized_keys"'
-	# cmd = [f'"{ssh}"','-p',f'{port}','-oStrictHostKeyChecking=no',f'{userhost}',
-	# 	f'"mkdir -p .ssh && echo \'{key}\' >> .ssh/authorized_keys && chmod 700 .ssh/authorized_keys"']
-	print(cmd)
-	# os.system(cmd)
+	# print(cmd)
 	user, host = userhost.split('@')
 	client = paramiko.SSHClient()
 	client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 	client.connect(hostname=host, username=user, password=password, port=port)
 	stdin, stdout, stderr = client.exec_command(cmd)
-	if test(ssh, userhost, port):
-		print("SSH setup successfull")
+	ok = checkssh(ssh, userhost, port)
+	if ok:
+		print("SSH setup successfull.")
 	else:
-		print('SSH setup failed')
+		print("SSH setup failed.")
+	return 0 if ok else 1
 
 
 if __name__ == '__main__':
@@ -75,5 +65,4 @@ if __name__ == '__main__':
 	if ':' in userhost:
 		userhost, port = userhost.split(':')		
 	ssh = r'C:\Program Files\SSHFS-Win\bin\ssh.exe'
-	keygen = r'C:\Program Files\SSHFS-Win\bin\ssh-keygen.exe'
-	main(ssh, keygen, userhost, password, port)
+	sys.exit(main(ssh, userhost, password, port))
