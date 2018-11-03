3afc33ec268ec8cd644cb16f3ae014c490553f1a
San <sganis@gmail.com>
2018-11-03 17:44:15 +0300

    Added fuse arguments

--- src/sanfs/main.c
+++ src/sanfs/main.c
@@ -237,7 +237,11 @@ int main(int argc, char *argv[])
 	printf("Threads needed: %d\n", san_threads(5, get_number_of_processors()));
 
 	// run fuse main
-	fuse_opt_add_arg(&args, "-ouid=197609,gid=-1,VolumePrefix=/sanfs/linux,rellinks,FileInfoTimeout=3000,DirInfoTimeout=3000");
+	fuse_opt_add_arg(&args, "-ouid=-1,gid=-1,create_umask=007,mask=007");
+	fuse_opt_add_arg(&args, "-oVolumePrefix=/sanfs/linux");
+	fuse_opt_add_arg(&args, "-ovolname=user@host");
+	fuse_opt_add_arg(&args, "-oFileSystemName=SANFS");
+	fuse_opt_add_arg(&args, "-orellinks,FileInfoTimeout=3000,DirInfoTimeout=3000");
 	fuse_opt_parse(&args, &sanfs, sanfs_opts, sanfs_opt_proc);
 	fuse_opt_add_arg(&args, sanfs.drive);
 	// debug arguments
