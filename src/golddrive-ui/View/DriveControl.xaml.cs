6175f07b79bc52bd8895054715d8837a95c915e5
San <sganis@gmail.com>
2019-03-23 18:25:09 +0300

    Added ico

---
+++ src/golddrive-ui/View/DriveControl.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace golddrive
+{
+    public partial class DriveControl : UserControl
+    {
+        public DriveControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
