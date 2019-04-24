3f046ca3b7da59fe082d6c257385b6d68712df5c
San <sganis@gmail.com>
2019-04-24 16:32:37 +0300

    App cmd line arguments

--- src/app/App.xaml.cs
+++ src/app/App.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,6 +12,25 @@ namespace golddrive
 {
     public partial class App : Application
     {
-        
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            if ( e.Args.Length > 0 )
+            {
+                string apppath = System.Reflection.Assembly.GetEntryAssembly().Location;
+                string appdir = System.IO.Path.GetDirectoryName(apppath);
+                Console.WriteLine("Running console...");
+                string cmd = $"{appdir}\\golddrive.exe";
+                string args = string.Join(" ", e.Args);
+                Console.WriteLine($"{cmd} {args}");
+                Process.Start(cmd, args);
+            }
+            else
+            {
+                new MainWindow().ShowDialog();
+            }
+            this.Shutdown();
+        }
     }
 }
