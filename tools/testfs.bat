525d9fa0765f4401fdd0246253ff0ec6dd8ca491
San <sganis@gmail.com>
2020-03-11 12:05:12 +0300

    Update testfs.bat

--- tools/testfs.bat
+++ tools/testfs.bat
@@ -31,8 +31,8 @@ set DIR=%DIR:~0,-1%
 del test test.*
 if !ERRORLEVEL! neq 0 goto fail
 
-%DIR%\fsbench-x64.exe -rdwr_cc_* -mmap_* ^
-	-file_attr* -file_list_single* -file_list_none* -rdwr_nc_*
+%DIR%\fsbench-x64.exe -rdwr_cc_* -mmap_* 
+::	-file_attr* -file_list_single* -file_list_none* -rdwr_nc_*
 if !ERRORLEVEL! neq 0 goto fail
 
 echo PASSED
