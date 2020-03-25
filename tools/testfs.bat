f88c777d21ff56a671090bfc09f98e2c2dbecb81
San <sganis@gmail.com>
2020-03-25 17:39:21 +0300

    Tracking libssh2 bug

--- tools/testfs.bat
+++ tools/testfs.bat
@@ -11,18 +11,18 @@ set DIR=%DIR:~0,-1%
 echo current dir:
 cd
 
-%DIR%\fstools\fsx.exe -N 5000 test xxxxxx
-rem %DIR%\fstools\fsx.exe -N 1000 -L test xxxxxx
+%DIR%\fstools\fsx.exe -N 1000 test xxxxxx
+rem %DIR%\fstools\fsx.exe -N 5000 -L test xxxxxx
 rem del test test.*
 echo error: !ERRORLEVEL!
 if !ERRORLEVEL! neq 0 goto fail
 
-%DIR%\fstools\fsbench-x64.exe -rdwr_cc_* -mmap_* ^
- -file_attr* -file_list_single* -file_list_none* -rdwr_nc_*
-if !ERRORLEVEL! neq 0 goto fail
+rem %DIR%\fstools\fsbench-x64.exe -rdwr_cc_* -mmap_* ^
+rem  -file_attr* -file_list_single* -file_list_none* -rdwr_nc_*
+rem if !ERRORLEVEL! neq 0 goto fail
 
-%DIR%\iozone\iozone.exe -i0 -i1 -i2 -s 1m -s10m -r1m
-if !ERRORLEVEL! neq 0 goto fail
+rem %DIR%\iozone\iozone.exe -i0 -i1 -i2 -s 1m -s10m -r1m
+rem if !ERRORLEVEL! neq 0 goto fail
 
 echo PASSED
 exit /b 0
