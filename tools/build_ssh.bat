a9cea60f45ab302ac81d875062df21367a8e64f2
San <sganis@gmail.com>
2020-03-29 13:58:58 +0300

    Libssh build with new openssl 1.1.1e no-zip

--- tools/build_ssh.bat
+++ tools/build_ssh.bat
@@ -9,17 +9,19 @@ set "MSVC=Visual Studio 16 2019"
 
 ECHO "Building with platform %MSVC%"
 cmake .. 								^
- -A x64 ^
- -DCRYPTO_BACKEND=OpenSSL               ^
+ -A x64 								^
  -G"%MSVC%"                             ^
- -DOPENSSL_ROOT_DIR="C:\openssl2"        ^
- -DZLIB_LIBRARY="C:\zlib\lib"              ^
- -DZLIB_INCLUDE_DIR="C:\zlib\include"              ^
+ -DOPENSSL_ROOT_DIR="C:/openssl2"       ^
  -DOPENSSL_MSVC_STATIC_RT=TRUE 			^
  -DOPENSSL_USE_STATIC_LIBS=TRUE			^
- -DBUILD_SHARED_LIBS=OFF         ^       
- -DWITH_ZLIB=OFF
+ -DBUILD_SHARED_LIBS=ON         		^
+ -DWITH_ZLIB=OFF ^
+ -DWITH_SERVER=OFF ^
+ -DCMAKE_BUILD_TYPE=Release
  
+:: -DZLIB_LIBRARY="C:\zlib\lib"              
+:: -DZLIB_INCLUDE_DIR="C:\zlib\include"              
+
 cmake --build . --config Release
 
-cd %DIR%
+::cd %DIR%
