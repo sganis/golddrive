3ab536ce505a9011ca84998e34b3f4fcdd651ce9
San <sganis@gmail.com>
2020-03-29 13:41:30 +0300

    Same but non-blocking

--- tools/build_ssh.bat
+++ tools/build_ssh.bat
@@ -9,17 +9,16 @@ set "MSVC=Visual Studio 16 2019"
 
 ECHO "Building with platform %MSVC%"
 cmake .. 								^
- -A x86 ^
- -DCMAKE_BUILD_TYPE=Release             ^
+ -A x64 ^
  -DCRYPTO_BACKEND=OpenSSL               ^
  -G"%MSVC%"                             ^
- -DOPENSSL_ROOT_DIR="C:\openssl"        ^
+ -DOPENSSL_ROOT_DIR="C:\openssl2"        ^
  -DZLIB_LIBRARY="C:\zlib\lib"              ^
  -DZLIB_INCLUDE_DIR="C:\zlib\include"              ^
  -DOPENSSL_MSVC_STATIC_RT=TRUE 			^
  -DOPENSSL_USE_STATIC_LIBS=TRUE			^
- -DBUILD_SHARED_LIBS=OFF                
-:: -DWITH_ZLIB=OFF
+ -DBUILD_SHARED_LIBS=OFF         ^       
+ -DWITH_ZLIB=OFF
  
 cmake --build . --config Release
 
