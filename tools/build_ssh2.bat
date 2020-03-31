8674bae5f3fbfd0cdb96c3ae607ead58523e1c59
San <sganis@gmail.com>
2020-03-31 20:59:53 +0300

    None cipher and none mac build in libssh2

--- tools/build_ssh2.bat
+++ tools/build_ssh2.bat
@@ -11,15 +11,24 @@ set DIR=%DIR:~0,-1%
 mkdir build && cd build
 
 ::set OPENSSL_DIR=%DIR%\..\src\lib\openssl
-set OPENSSL_DIR=C:\openssl2
+set OPENSSL_DIR=C:/Users/Sant/Documents/golddrive/src/lib/openssl
+set ZLIB_DIR="C:/zlib-x64"
 
 cmake .. ^
- -DCRYPTO_BACKEND=OpenSSL               ^
- -DBUILD_SHARED_LIBS=OFF                ^
- -DOPENSSL_ROOT_DIR=%OPENSSL_DIR% 		^
- -DOPENSSL_MSVC_STATIC_RT=TRUE 			^
- -DOPENSSL_USE_STATIC_LIBS=TRUE 		^
- -DENABLE_ZLIB_COMPRESSION=OFF
-cmake --build . --config Release
+ -DCMAKE_INSTALL_PREFIX="C:/libssh2-x64"		^
+ -DCRYPTO_BACKEND=OpenSSL               		^
+ -DBUILD_SHARED_LIBS=OFF                		^
+ -DOPENSSL_ROOT_DIR=%OPENSSL_DIR% 				^
+ -DOPENSSL_MSVC_STATIC_RT=TRUE 					^
+ -DOPENSSL_USE_STATIC_LIBS=TRUE 				^
+ -DENABLE_ZLIB_COMPRESSION=ON 					^
+ -DBUILD_TESTING=OFF 							^
+ -DBUILD_EXAMPLES=OFF %DOPEN_SSL_STATIC%		^
+ -DENABLE_CRYPT_NONE=ON							^
+ -DENABLE_MAC_NONE=ON ^
+ -DZLIB_LIBRARY=%ZLIB_DIR%/lib/zlibstatic.lib   ^
+ -DZLIB_INCLUDE_DIR=%ZLIB_DIR%/include 			
+
+cmake --build . --config Release  --target install -- /clp:ErrorsOnly
 
 
--- tools/build_zlib.bat
+++ tools/build_zlib.bat
@@ -2,18 +2,18 @@ mkdir build && cd build
 
 set "MSVC=Visual Studio 16 2019"
 
-set ARCH=Win32
-set PLAT=x86
+set ARCH=x64
+set PLAT=x64
 
 :: zlib
 :: Download http://zlib.net/zlib1211.zip
 cmake ..                                         ^
     -A %ARCH% 									 ^
     -G"%MSVC%"                                   ^
-    -DCMAKE_INSTALL_PREFIX="C:\zlib-%PLAT%"      ^
+    -DCMAKE_INSTALL_PREFIX="C:/zlib-%PLAT%"      ^
     -DBUILD_SHARED_LIBS=OFF
 
-:: cmake --build . --config Release --target install
+ cmake --build . --config Release --target install
 ::copy C:/zlib/lib/zlibstatic.lib ../lib/
 ::cd ..
 ::rd /s zlib-1.2.11
