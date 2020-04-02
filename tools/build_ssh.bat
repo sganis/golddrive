a83f746ed345d9b315579caf1294cc97d0a3b0ee
San <sganis@gmail.com>
2020-04-02 21:11:53 +0300

    Added more reclen iozone

--- tools/build_ssh.bat
+++ tools/build_ssh.bat
@@ -6,12 +6,13 @@ set DIR=%DIR:~0,-1%
 
 mkdir build && cd build
 set "MSVC=Visual Studio 16 2019"
+set OPENSSL_DIR=C:/Users/Sant/Documents/golddrive/src/lib/openssl
 
 ECHO "Building with platform %MSVC%"
 cmake .. 								^
  -A x64 								^
  -G"%MSVC%"                             ^
- -DOPENSSL_ROOT_DIR="C:/openssl2"       ^
+ -DOPENSSL_ROOT_DIR=%OPENSSL_DIR%       ^
  -DOPENSSL_MSVC_STATIC_RT=TRUE 			^
  -DOPENSSL_USE_STATIC_LIBS=TRUE			^
  -DBUILD_SHARED_LIBS=ON         		^
@@ -22,6 +23,6 @@ cmake .. 								^
 :: -DZLIB_LIBRARY="C:\zlib\lib"              
 :: -DZLIB_INCLUDE_DIR="C:\zlib\include"              
 
-cmake --build . --config Release
+cmake --build . --target install -- /clp:ErrorsOnly
 
 ::cd %DIR%
