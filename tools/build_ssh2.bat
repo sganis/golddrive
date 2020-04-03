ef4478b4f9f8c5ede15bba4c5dbd1a22f3a2b42e
San <sganis@gmail.com>
2020-04-03 20:52:18 +0300

    Libssh with openssl no-stdio no-sock, but rsa keys dont work anymore

--- tools/build_ssh2.bat
+++ tools/build_ssh2.bat
@@ -19,14 +19,14 @@ set ZLIB_DIR="C:/zlib-x64"
 cmake .. ^
  -DCMAKE_INSTALL_PREFIX="C:/libssh2-x64"		^
  -DCMAKE_BUILD_TYPE=Release						^
- -DCRYPTO_BACKEND=WinCNG                 		^
+ -DCRYPTO_BACKEND=OpenSSL               		^
  -DBUILD_SHARED_LIBS=OFF                 		^
  -DOPENSSL_ROOT_DIR=%OPENSSL_DIR% 				^
  -DENABLE_ZLIB_COMPRESSION=OFF 					^
  -DBUILD_TESTING=OFF 							^
- -DBUILD_EXAMPLES=OFF 							
+ -DBUILD_EXAMPLES=OFF 							^
+ -DENABLE_CRYPT_NONE=ON							
  
- rem -DENABLE_CRYPT_NONE=ON							^
  rem -DOPENSSL_MSVC_STATIC_RT=TRUE 					^
  rem -DOPENSSL_USE_STATIC_LIBS=TRUE 
  rem ::-DZLIB_LIBRARY=%ZLIB_DIR%/lib/zlibstatic.lib   ^
