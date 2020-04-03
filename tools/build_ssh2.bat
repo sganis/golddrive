1e0e1bde89821638cb5fc5b004c62528800e8634
San <sganis@gmail.com>
2020-04-03 19:23:30 +0300

    Libssh2 with WinCNG

--- tools/build_ssh2.bat
+++ tools/build_ssh2.bat
@@ -19,17 +19,18 @@ set ZLIB_DIR="C:/zlib-x64"
 cmake .. ^
  -DCMAKE_INSTALL_PREFIX="C:/libssh2-x64"		^
  -DCMAKE_BUILD_TYPE=Release						^
- -DCRYPTO_BACKEND=OpenSSL               		^
+ -DCRYPTO_BACKEND=WinCNG                 		^
  -DBUILD_SHARED_LIBS=OFF                 		^
  -DOPENSSL_ROOT_DIR=%OPENSSL_DIR% 				^
  -DENABLE_ZLIB_COMPRESSION=OFF 					^
  -DBUILD_TESTING=OFF 							^
- -DBUILD_EXAMPLES=OFF 							^
- -DENABLE_CRYPT_NONE=ON							^
- -DOPENSSL_MSVC_STATIC_RT=TRUE 					^
- -DOPENSSL_USE_STATIC_LIBS=TRUE 
- ::-DZLIB_LIBRARY=%ZLIB_DIR%/lib/zlibstatic.lib   ^
- ::-DZLIB_INCLUDE_DIR=%ZLIB_DIR%/include 			
+ -DBUILD_EXAMPLES=OFF 							
+ 
+ rem -DENABLE_CRYPT_NONE=ON							^
+ rem -DOPENSSL_MSVC_STATIC_RT=TRUE 					^
+ rem -DOPENSSL_USE_STATIC_LIBS=TRUE 
+ rem ::-DZLIB_LIBRARY=%ZLIB_DIR%/lib/zlibstatic.lib   ^
+ rem ::-DZLIB_INCLUDE_DIR=%ZLIB_DIR%/include 			
 
 cmake --build . --config Release --target install
 :: -- /clp:ErrorsOnly
