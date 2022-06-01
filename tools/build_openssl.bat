7aae3620ddf7df1a2071ddda843006539cb4861e
San <sganis@gmail.com>
2022-06-01 13:48:56 +0300

    Added image vs 2022 in CI

--- tools/build_openssl.bat
+++ tools/build_openssl.bat
@@ -8,14 +8,26 @@ setlocal EnableDelayedExpansion
 set DIR=%~dp0
 set DIR=%DIR:~0,-1%
 
-
+nmake /f makefile.vc clean
 perl Configure 			    ^
 	VC-WIN64A 				^
 	no-shared               ^
-	--prefix=C:\openssl-3	^
-	--openssldir=C:\openssl-3
+	--prefix=C:\openssl-x64	^
+	--openssldir=C:\openssl-x64
 
-rem nmake build_libs
-rem nmake install_dev
+nmake build_libs
+nmake install_dev
+xcopy C:\openssl-x64\lib\libcrypto.lib ..\vendor\openssl\lib\x64 /y /s /i
+xcopy C:\openssl-x64\include ..\vendor\openssl\include /y /s /i
 
+nmake /f makefile.vc clean
+perl Configure 			    ^
+	VC-WIN32 				^
+	no-shared               ^
+	--prefix=C:\openssl-x86	^
+	--openssldir=C:\openssl-x86
+
+nmake build_libs
+nmake install_dev
+xcopy C:\openssl-x86\lib\libcrypto.lib ..\vendor\openssl\lib\x86 /y /s /i
 
--- tools/build_ssh2.bat
+++ tools/build_ssh2.bat
@@ -8,30 +8,51 @@ setlocal EnableDelayedExpansion
 set DIR=%~dp0
 set DIR=%DIR:~0,-1%
 
-mkdir build && cd build
 
 ::set OPENSSL_DIR=%DIR%\..\src\lib\openssl
 ::set OPENSSL_DIR=C:/Users/Sant/Documents/golddrive/src/lib/openssl/lib/x64
 rem set OPENSSL_DIR=C:/libre-x64
-set OPENSSL_DIR=C:/openssl-3
+rem set OPENSSL_DIR=C:/openssl-3
 rem set OPENSSL_DIR=C:/Users/Sant/Documents/winlibs/prefix/openssl-x64
 rem set OPENSSL_DIR=C:/Openssl
 ::set ZLIB_DIR="C:/zlib-x64"
 ::set CL=/DOPENSSL_NO_ENGINE=1 %CL%
 
+mkdir build_x64 && cd build_x64
 cmake .. ^
+ -A x64 ^
  -DCMAKE_INSTALL_PREFIX="C:/libssh2-x64"		^
  -DCMAKE_BUILD_TYPE=Release						^
  -DCRYPTO_BACKEND=OpenSSL               		^
  -DBUILD_SHARED_LIBS=OFF                 		^
- -DOPENSSL_ROOT_DIR=%OPENSSL_DIR% 				^
+ -DOPENSSL_ROOT_DIR=C:/openssl-x64 				^
  -DENABLE_ZLIB_COMPRESSION=OFF 					^
  -DBUILD_TESTING=OFF 							^
  -DBUILD_EXAMPLES=OFF 							^
  -DENABLE_CRYPT_NONE=ON							^
  -DCLEAR_MEMORY=OFF
-
-
 cmake --build . --config Release --target install
+
+xcopy C:\libssh2-x64\lib\libssh2.lib ..\vendor\libssh\lib\x64 /y /s /i
+xcopy C:\libssh2-x64\include ..\vendor\libssh\include /y /s /i
+cd ..
+
+mkdir build_x86 && cd build_x86
+cmake .. ^
+ -A Win32 ^
+ -DCMAKE_INSTALL_PREFIX="C:/libssh2-x86"        ^
+ -DCMAKE_BUILD_TYPE=Release                     ^
+ -DCRYPTO_BACKEND=OpenSSL                       ^
+ -DBUILD_SHARED_LIBS=OFF                        ^
+ -DOPENSSL_ROOT_DIR=C:/openssl-x86              ^
+ -DENABLE_ZLIB_COMPRESSION=OFF                  ^
+ -DBUILD_TESTING=OFF                            ^
+ -DBUILD_EXAMPLES=OFF                           ^
+ -DENABLE_CRYPT_NONE=ON                         ^
+ -DCLEAR_MEMORY=OFF
+cmake --build . --config Release --target install
+
+xcopy C:\libssh2-x86\lib\libssh2.lib ..\vendor\libssh\lib\x86 /y /s /i
+rem xcopy C:\libssh2-x86\include ..\vendor\libssh\include /y /s /i
 
 cd ..
